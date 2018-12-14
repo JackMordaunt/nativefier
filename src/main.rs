7dd732695831af8e0986e190c54e5226228607e2
Jack Mordaunt <jackmordaunt@gmail.com>
2018-12-14 20:19:03 +1300

    [+] Deliniate between the two execution modes.\nPackage is implemented for MacOS.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,60 @@
 use web_view::*;
 use clap::{Arg, App};
+use std::{
+    env,
+    fs,
+    path::PathBuf,
+    io::prelude::*,
+    error::Error,
+};
 use handlebars::Handlebars;
 use serde_json::json;
 
-fn main() {
+// TODO(jfm): Template this out with real app name. 
+static PLIST: &'static str = r#"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleDevelopmentRegion</key>
+	<string>English</string>
+	<key>CFBundleExecutable</key>
+	<string>{{executable}}</string>
+	<key>CFBundleIdentifier</key>
+	<string>com.nativefier.{{executable}}</string>
+	<key>CFBundleName</key>
+	<string>{{executable}}</string>
+	<key>CFBundleSupportedPlatforms</key>
+	<array>
+		<string>MacOSX</string>
+	</array>
+	<key>NSSupportsSuddenTermination</key>
+	<string>YES</string>
+        <key>NSHighResolutionCapable</key>
+        <string>True</string>
+</dict>
+</plist>
+
+"#;
+
+enum Mode {
+    Generator,
+    Generated,
+}
+
+impl Mode {
+    fn detect() -> Result<Mode, Box<Error>> {
+        // TODO(jfm): implement for Windows/Linux.
+        for c in env::current_exe()?.canonicalize()?.components() {
+            if c.as_os_str().to_string_lossy().contains(".app") {
+                return Ok(Mode::Generated);
+            }
+        }
+        return Ok(Mode::Generator);
+    }
+}
+
+fn generator() {
     let matches = App::new("nativefier")
         .version("0.0.1")
         .author("Jack Mordaunt <jackmordaunt@gmail.com>")
@@ -17,37 +68,45 @@ fn main() {
             .takes_value(true)
             .help("url of site to nativefy"))
         .get_matches();
-    let title = matches.value_of("title").expect("parsing title flag");
-    let url = matches.value_of("url").expect("parsing url flag");
-    let content = Handlebars::new().render_template(HTML, &json!({"title": title, "href": url})).unwrap();
-    web_view::builder()
-        .title(title)
-        .content(Content::Html(content))
+    let title = matches.value_of("title").expect("parsing title");
+    let url = matches.value_of("url").expect("parsing url");
+    // TODO(jfm): bundle for Windows/Linux.
+    bundle_darwin(title, url).expect("bundling site into MacOS .app bundle");
+}
+
+fn bundle_darwin(title: &str, url: &str) -> Result<(), Box<Error>> {
+    let app = PathBuf::from(format!("{0}.app/Contents/MacOS/{0}", title));
+    let plist = PathBuf::from(format!("{0}.app/Contents/Info.plist", title));
+    fs::create_dir_all(app.parent().unwrap())?;
+    fs::copy(env::current_exe()?.to_path_buf(), app)?;
+    let h = Handlebars::new();
+    fs::File::create(plist)?.write(h.render_template(PLIST, &json!({"executable": title}))?.as_bytes())?;
+    // TODO(jfm): Write wrapper bash script to pass url to the binary as a flag.  
+    Ok(())
+}
+
+fn generated() {
+    let (title, url) = find_config().unwrap();
+    let wv = web_view::builder()
+        .title(&title)
+        .content(Content::Url(&url))
         .size(800, 600)
         .resizable(true)
         .debug(true)
         .user_data(())
-        .invoke_handler(|_webview, _arg| Ok(()))
-        .run()
-        .expect("running webview");
+        .invoke_handler(|_wv, _arg| { Ok(()) } )
+        .build()
+        .expect("building webview");
+    wv.run().expect("running webview");
 }
 
+fn find_config() -> Result<(String, String), Box<Error>> {
+    Ok((String::from("SoundCloud"), String::from("https://soundcloud.com/discover")))
+}
 
-const HTML: &str = r#"
-<!doctype html>
-<html>
-    <head>
-        <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise,fetch,Symbol,Array.prototype.@@iterator"></script> 
-        <script>
-            var script = document.createElement("script")
-            script.setAttribute("type", "text/javascript")
-            script.setAttribute("src", "https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise,fetch,Symbol,Array.prototype.@@iterator")
-            window.location = "{{href}}"
-        </script>
-    </head>
-    <body>
-        <h1>{{title}}</h1>
-        <iframe src="{{href}}">{{title}}</iframe>
-    </body>
-</html>
-"#;
+fn main() {
+    match Mode::detect().expect("detecting execution mode") {
+        Mode::Generator => generator(),
+        Mode::Generated => generated(),
+    }
+}
