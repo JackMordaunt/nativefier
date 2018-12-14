31a155069232106293d5112d02ea3919ea688ad2
Jack Mordaunt <jackmordaunt@gmail.com>
2018-12-13 23:28:39 -0800

    [-] Todo completed.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use std::{
 use handlebars::Handlebars;
 use serde_json::json;
 
-// TODO(jfm): Template this out with real app name. 
 static PLIST: &'static str = r#"
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
