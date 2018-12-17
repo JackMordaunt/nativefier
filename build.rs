21b71839eaa36ae89cc15d8619411dbd6c8147b0
Jack Mordaunt <jackmordaunt@gmail.com>
2018-12-16 16:27:30 -0800

    [+] Compile binary with icon! (windows)

---
+++ build.rs
@@ -0,0 +1,27 @@
+use winres;
+use image::{
+	imageops,
+};
+use std::path::Path;
+
+#[cfg(windows)]
+fn main() {
+	// Check for icon.ico
+	// if icon.ico doesnt exist, convert icon.png to icon.ico
+	if !Path::new("assets/icon.ico").exists() {
+		if Path::new("assets/icon.png").exists() {
+			let src = image::open("assets/icon.png").unwrap();
+			let resized = imageops::resize(&src, 255, 255, imageops::Lanczos3);
+			resized.save("assets/icon.ico").unwrap();
+		}
+	}
+	let mut res = winres::WindowsResource::new();
+	res.set_icon("assets/icon.ico");
+	res.compile().expect("compiling winres");
+}
+
+#[cfg(unix)]
+fn main() {
+	
+}
+
