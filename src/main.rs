70692eca7129e92f0f56fb32d72ce99c7525409a
Jack Mordaunt <jackmordaunt@gmail.com>
2019-01-02 12:22:42 +1300

    [~] Remove commented code.

--- src/main.rs
+++ src/main.rs
@@ -49,11 +49,6 @@ fn main() {
                 None => ("", url),
             };
             let icon = infer_icon(&icon_url).expect("inferring icon");
-            // let icon_path = tempdir()
-            //     .expect("opening temporary directory")
-            //     .into_path()
-            //     .join(format!("icon.{}", &icon.ext));
-            // fs::write(&icon_path, &icon).expect("writing icon to disk");
             if cfg!(windows) {
                 bundle::Windows {
                     dir: &dir,
