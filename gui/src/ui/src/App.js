ebb5d551128f8fcb4afc1ef0180668eff606c63d
= <=>
2019-09-04 03:08:09 +1200

    [+] Initialise react ui.

---
+++ gui/src/ui/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <h1>Hello Nativefier</h1>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
